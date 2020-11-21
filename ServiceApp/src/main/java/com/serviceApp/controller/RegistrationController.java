befb35beb934a1a35554e0c748b29bf6e417fafa
X-workzDev01 <xworkzdev01@gmail.com>
2020-11-21 12:13:55 +0530

    Addedd : crosssOrigin in controller

--- ServiceApp/src/main/java/com/serviceApp/controller/RegistrationController.java
+++ ServiceApp/src/main/java/com/serviceApp/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.serviceApp.service.RegistrationService;
 import com.serviceApp.utility.response.Response;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class RegistrationController {
 
